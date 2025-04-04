e420afe986df5659e0e7bc5edea3c364de9aa899
Andres Barrios Arenas <andres@generaweb.net>
2025-04-04 10:40:15 -0600

    Gráficas en cuadernos

--- src/pages/Cuadernos/DetalleCuaderno.jsx
+++ src/pages/Cuadernos/DetalleCuaderno.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
+import { Chart } from 'react-chartjs-2';
 import { selectNewYear } from '../../parametersSlice'
 import './DetalleCuaderno.css'
 import Header from '../Header';
@@ -37,6 +38,34 @@ function DetalleCuaderno() {
     },{
         texto: "Cuaderno"
     }]);
+    const chartRef = useRef(null);
+    const [configChart, setConfigChart] = useState({
+        labels: [],
+        datasets: [],
+    });
+    const optionsChart = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+            },
+            title: {
+                display: false
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = ' '+context.dataset.label || ' ';
+                        if (context.parsed.y !== null) {
+                            label += ': '+ (context.parsed.y).toLocaleString("en-MX", {style:"decimal",maximumFractionDigits:2, minimumFractionDigits: 2});
+                        }
+                        return label;
+                    }
+                }
+            }
+        },
+    };
+    const _colores=["#4E79A7","#A0CBE8","#F28E2B","#FFBE7D","#59A14F","#8CD17D","#B6992D","#F1CE63","#499894","#86BCB6","#E15759","#FF9D9A","#79706E","#BAB0AC","#D37295","#FABFD2","#B07AA1","#D4A6C8","#9D7660","#D7B5A6"];
 
     // obtener el detalle del cuaderno
     useEffect( () => {
@@ -84,6 +113,18 @@ function DetalleCuaderno() {
                 let result={};
                 result.Id=renglon.Id;
                 result.Mostrar=renglon.Mostrar;
+                result.Graph=renglon.Graph;
+                result.label="";
+                renglon.Filtro ? result.label+=renglon.Filtro.Clave+"-"+renglon.Filtro.Nombre : null;
+                if(renglon.TipoFiltro=="Estado"){
+                        let estado=estados.filter((itemEstado) => { return itemEstado.Id == parseInt(renglon.Estado) })
+                        result.label+=estado[0].Nombre
+                };
+                renglon.Tipo=="ProgramaPresupuestal" ? result.label+=renglon.Referencia.Clave+"-"+renglon.Referencia.Nombre : null;
+                renglon.Tipo!=="Total" && renglon.Tipo!=="ProgramaPresupuestal" ?
+                    result.label+=': '+renglon.Referencia.Clave+' - '+renglon.Referencia.Nombre
+                    : null;
+
                 let versionesEstado=datosCuaderno.VersionesPresupuesto.filter((versiones) => {
                     return versiones.estado == renglon.Estado
                 });
@@ -147,6 +188,45 @@ function DetalleCuaderno() {
             setDatosTabla(data);
         }
     },[selectedYear,datosCuaderno]);
+
+    // Actualizar gráfica
+    useEffect(() => {
+        if(datosTabla.length>0){
+            let labels=datosCuaderno.Anios.map((anio) => {
+                return anio.Anio
+            })
+            let datasets=datosTabla.map((renglon,index) => {
+                console.log(renglon)
+                let data=[];
+                if(renglon.Mostrar=="YoY"){
+                    data=renglon.datos.map((dato) => {
+                        return dato.monto-dato.montoAnterior
+                    })
+                }else{
+                    data=renglon.datos.map((dato) => {
+                        return dato
+                    })
+                }
+                let dataset={
+                    label: renglon.label,
+                    data: data,
+                    borderColor: _colores[index],
+                    backgroundColor: _colores[index],
+                    fill: false,
+                    tension: 0.3,
+                  };
+                return dataset;
+            })
+            const chart = chartRef.current;
+            if (chart) {
+                setConfigChart({
+                    labels: labels,
+                    datasets: datasets
+                })
+                chart.update();
+            }
+        }
+    }, [datosTabla]);
 
     // Rellenar la tabla
     const rowsTablaDatos = () => {
@@ -267,6 +347,14 @@ function DetalleCuaderno() {
                     })
                 }
             </ul>
+        </div>
+        <div className='mb-4' id='chart'>
+            <Chart 
+                ref={chartRef}
+                type='line' 
+                data={configChart}  
+                options={optionsChart}
+            />
         </div>
         <div className='datos'>
             <h3>Datos</h3>
