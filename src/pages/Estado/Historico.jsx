6b37586fa017c03522f703cf5fe7d321c950380b
Andres Barrios Arenas <andres@generaweb.net>
2025-04-08 17:43:58 -0600

    chart diferencias histórico

--- src/pages/Estado/Historico.jsx
+++ src/pages/Estado/Historico.jsx
@@ -9,7 +9,9 @@ const Historico = ({estadoActual}) => {
     const inpc = useSelector(state => state.parameters.inpc)
     const [historico,setHistorico] = useState([]);
     const [rowsTabla,setRowsTabla] = useState([]);
+    const [showGraphPresupuesto, setShowGraphPresupuesto] = useState('historico');
     const chartRef = useRef(null);
+    const chartRefDiferencias = useRef(null);
     const [configChart, setConfigChart] = useState({
         labels: [],
         datasets: [{
@@ -38,6 +40,48 @@ const Historico = ({estadoActual}) => {
             }
         },
     };
+    const [configChartDiferencias, setConfigChartDiferencias] = useState({
+        labels: [],
+        datasets: [{
+            label: 'Diferencia anual',
+            data: [],
+            borderColor: 'rgb(140, 180, 193)',
+            backgroundColor: 'rgba(140, 180, 193, 0.5)',
+            fill: true
+          },],
+    });
+    const optionsChartDiferencias = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Diferencia anual',
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = ' ';
+                        if (context.parsed.y !== null) {
+                            label += (context.parsed.y).toFixed(2) + '%';
+                        }
+                        return label;
+                    }
+                }
+            }
+        },
+        scales: {
+            y: {
+                ticks: {
+                    callback: function(value, index, ticks) {
+                        return value + '%';
+                    }
+                }
+            }
+        }
+    };
 
     useEffect(() => {
         if(api_url && estadoActual.Codigo){
@@ -58,7 +102,11 @@ const Historico = ({estadoActual}) => {
         if(historico.length>0){
             let labels = [];
             let data = [];
+            let dataDiferencias = [];
+            let dataChartBorderColor=[];
+            let dataChartBackgroundColor=[];
             let rows=[];
+            
             historico.map((version) => {
                 labels.push(version.Anio);
                 data.push(version.Monto*inpc[selectedYear]/inpc[version.Anio]);
@@ -69,8 +117,16 @@ const Historico = ({estadoActual}) => {
                 if(index>0){
                     let montoAnterior=historico[index-1].Monto*inpc[selectedYear]/inpc[historico[index-1].Anio];
                     diferencia= (monto-montoAnterior)/montoAnterior;
+                    dataDiferencias.push(diferencia*100);
+                    if(diferencia>0){
+                        dataChartBorderColor.push('rgb(140, 180, 193)');
+                        dataChartBackgroundColor.push('rgba(140, 180, 193, 0.5)');
+                    }else{
+                        dataChartBorderColor.push('rgb(217, 91, 91)');
+                        dataChartBackgroundColor.push('rgba(217, 91, 91, 0.5)');
+                    }
                 }
-                return(<><tr>
+                return(<><tr key={'tr_'+version.Anio}>
                     <td>{version.Anio} <small>{version.Tipo}</small></td>
                     <td className='font-monospace text-end'>
                     {monto.toLocaleString("en-MX", {style:"decimal",maximumFractionDigits:2, minimumFractionDigits: 2})}
@@ -91,6 +147,18 @@ const Historico = ({estadoActual}) => {
             if(chartRef.current){
                 chartRef.current.update();
             }   
+            let labelsDiferencias = [...labels]
+            labelsDiferencias.shift();
+            setConfigChartDiferencias({
+                labels: labelsDiferencias,
+                datasets: [{...configChartDiferencias.datasets[0], 
+                    data: dataDiferencias,
+                    borderColor: dataChartBorderColor,
+                    backgroundColor: dataChartBackgroundColor}],
+            });
+            if(chartRefDiferencias.current){
+                chartRefDiferencias.current.update();
+            }
         }
     },[historico,selectedYear]);
     return(<>
@@ -99,6 +167,7 @@ const Historico = ({estadoActual}) => {
             <h3>Histórico</h3>
         </div>
         <div className='col-12 col-md-6 mb-4'>
+            { showGraphPresupuesto === 'historico' ? (<>
             <Chart
                 ref={chartRef}
                 type='line' 
@@ -106,10 +175,37 @@ const Historico = ({estadoActual}) => {
                 options={optionsChart}
                 id='chartHistorico'
                 width='100%'
-                height={'70vw'}
+                height={'80vw'}
             />
+            </>) : (<>
+            <Chart
+                ref={chartRefDiferencias}
+                type='bar' 
+                data={configChartDiferencias}  
+                options={optionsChartDiferencias}
+                id='chartDiferencias'
+                width='100%'
+                height={'80vw'}
+            />
+            </>) }
         </div>
         <div className='col-12 col-md-6 mb-4'>
+        <div className='text-end opcionesGraph mb-2'>
+                <button 
+                    className={ showGraphPresupuesto=='historico' ? 'btn btn-outline-secondary active' : 'btn btn-outline-secondary ' }
+                    title='Presupuesto histórico'
+                    onClick={()=>setShowGraphPresupuesto('historico')}
+                    >
+                    <span className="material-symbols-outlined">show_chart</span>
+                </button>
+                <button 
+                    className={ showGraphPresupuesto=='diferencia' ? 'btn btn-outline-secondary active' : 'btn btn-outline-secondary ' }  
+                    title='Diferencia anual'
+                    onClick={()=>setShowGraphPresupuesto('diferencia')}
+                    >
+                    <span className="material-symbols-outlined">waterfall_chart</span>
+                </button>
+            </div>
             <table className='table table-bordered table-responsive' id='tablaHistorico'>
                 <thead>
                     <tr>
